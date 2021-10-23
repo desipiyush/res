ccdd4aab1490abc9914c151cb7f05569f7fe5d60
desipiyush <89911392+desipiyush@users.noreply.github.com>
2021-10-23 17:15:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var good = document.getElementById("a");
+var bad = document.getElementById("b");
+var circle = document.getElementById("circle");
+
+bad.classList.toggle("s");
+function b() {
+  if(xad.paused){
+    xad.play();
+  }
+  else{
+    xad.pause();
+  }
+  bad.classList.toggle("s");
+  good.classList.toggle("v");
+  circle.classList.toggle("active");
+}
